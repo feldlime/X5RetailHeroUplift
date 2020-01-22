926532bb6e92eeddce774e328e1177afd78b7d4a
feldlime <feldlime@yandex.ru>
2020-01-23 01:53:22 +0300

    refactored

--- models/fit_predict.py
+++ models/fit_predict.py
@@ -1,5 +1,18 @@
 from sklearn.base import clone
 
+
+def make_z(treatment, target):
+    y = target
+    w = treatment
+    z = y * w + (1 - y) * (1 - w)
+    return z
+
+
+def calc_uplift(prediction):
+    uplift = 2 * prediction - 1
+    return uplift
+
+
 def uplift_fit(model, X_train, treatment_train, target_train):
     """
     Реализация чуть более сложного способа построения uplift-модели.
@@ -12,9 +25,7 @@ def uplift_fit(model, X_train, treatment_train, target_train):
     Uplift считаем по формуле (Бабушкин 5:34):
     Predicted Uplift = 2 * P(Z=1) - 1
     """
-    y = target_train
-    w = treatment_train
-    z = y * w + (1 - y) * (1 - w)
+    z = make_z(treatment_train, target_train)
 
     model = clone(model)
     model.fit(X_train, z)
@@ -24,5 +35,5 @@ def uplift_fit(model, X_train, treatment_train, target_train):
 
 def uplift_predict(model, X_test):
     predict_z = model.predict_proba(X_test)[:, 1]
-    predict_uplift = 2 * predict_z - 1
-    return predict_uplift
+    uplift = calc_uplift(predict_z)
+    return uplift
