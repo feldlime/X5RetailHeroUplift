30d835c1fe2d0ef177d3b338da90587171779bdb
feldlime <feldlime@yandex.ru>
2020-01-22 22:33:03 +0300

    added metric tools

---
+++ models/metrics.py
@@ -0,0 +1,47 @@
+from typing import Dict
+
+import numpy as np
+from sklearn.metrics import roc_auc_score
+
+
+def score_uplift(
+    prediction: np.ndarray,
+    treatment: np.ndarray,
+    target: np.ndarray,
+    rate: float = 0.3,
+) -> float:
+    """
+    Подсчет Uplift Score
+    """
+    order = np.argsort(-prediction)
+    treatment_n = int((treatment == 1).sum() * rate)
+    treatment_p = target[order][treatment[order] == 1][:treatment_n].mean()
+    control_n = int((treatment == 0).sum() * rate)
+    control_p = target[order][treatment[order] == 0][:control_n].mean()
+    score = treatment_p - control_p
+    return score
+
+
+def score_roc_auc(
+    prediction: np.ndarray,
+    treatment: np.ndarray,
+    target: np.ndarray,
+) -> float:
+    y = target
+    w = treatment
+    z = y * w + (1 - y) * (1 - w)
+    score = roc_auc_score(z, prediction)
+    return score
+
+
+def uplift_metrics(
+    prediction: np.ndarray,
+    treatment: np.ndarray,
+    target: np.ndarray,
+    rate_for_uplift: float = 0.3,
+) -> Dict[str, float]:
+    scores = {
+        'roc_auc': score_roc_auc(prediction, treatment, target),
+        'uplift': score_uplift(prediction, treatment, target, rate_for_uplift),
+    }
+    return scores
--- utils.py
+++ utils.py
@@ -11,14 +11,4 @@ SUBMISSIONS_PATH = pjoin(PROJECT_PATH, 'submissions')
 
 
 
-def uplift_score(prediction, treatment, target, rate=0.3):
-    """
-    Подсчет Uplift Score
-    """
-    order = np.argsort(-prediction)
-    treatment_n = int((treatment == 1).sum() * rate)
-    treatment_p = target[order][treatment[order] == 1][:treatment_n].mean()
-    control_n = int((treatment == 0).sum() * rate)
-    control_p = target[order][treatment[order] == 0][:control_n].mean()
-    score = treatment_p - control_p
-    return score
+
